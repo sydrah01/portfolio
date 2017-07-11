7b41427898c19f1a40a9dc7c72870e4e9bd5763b
sydrah <sydrah10@yahoo.com>
2017-07-11 10:34:21 -0700

    save

---
+++ BlinkLED.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(12, HIGH);   
+  delay(200);              
+  digitalWrite(12, LOW);
+  delay(200);              
+}
+
+if pinMode= 12 {
+  digitalWrite(13, HIGH);
+}
+
+
+if pinMode= 13 {
+   digitalWrite(12,HIGH);
+}
+
